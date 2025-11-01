157679236146d8fca0f88630e9aaec5381568311
Dasioreq <adammarzec112@gmail.com>
2025-11-01 13:01:45 +0100

    feat: add basic ship ui

---
+++ scripts/shipSelection.js
@@ -0,0 +1,72 @@
+$(document).ready(function()
+{
+    $("body").mousemove(function(event)
+    {
+        $("div#ship").css({"transform": `translate(${-event.pageX / $(window).width() * 5 + 2.5}%, ${-event.pageY / $(window).height() * 5 + 2.5}%)`})
+    })
+
+    $("div#ship > div#crew").click(function(event)
+    {
+        console.log("crew");
+    })
+    $("div#ship > div#crew").mouseenter(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/ShipCrew.png")
+    })
+    $("div#ship > div#crew").mouseleave(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/Ship.png")
+    })
+
+    $("div#ship > div#fire").click(function(event)
+    {
+        console.log("fire");
+    })
+    $("div#ship > div#fire").mouseenter(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/ShipFire.png")
+    })
+    $("div#ship > div#fire").mouseleave(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/Ship.png")
+    })
+
+    $("div#ship > div#infirmary").click(function(event)
+    {
+        console.log("infirmary");
+    })
+    $("div#ship > div#infirmary").mouseenter(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/ShipInfirmary.png")
+    })
+    $("div#ship > div#infirmary").mouseleave(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/Ship.png")
+    })
+
+    $("div#ship > div#recharge").click(function(event)
+    {
+        console.log("recharge");
+    })
+    $("div#ship > div#recharge").mouseenter(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/ShipRecharge.png")
+    })
+    $("div#ship > div#recharge").mouseleave(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/Ship.png")
+    })
+
+    $("div#ship > div#repair").click(function(event)
+    {
+        console.log("repair");
+    })
+    $("div#ship > div#repair").mouseenter(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/ShipRepair.png")
+    })
+    $("div#ship > div#repair").mouseleave(function(event)
+    {
+        $("div#ship > img").attr("src", "../assets/Ship.png")
+    })
+})
